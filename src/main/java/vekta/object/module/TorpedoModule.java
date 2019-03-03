571902d25072f67993193dc5c4ac8b894a409c5f
StGerGer <nateestes223@gmail.com>
2019-03-02 20:06:07 -0700

    Added Torpedo model, fixed initial heading of torpedo

--- src/main/java/vekta/object/module/TorpedoModule.java
+++ src/main/java/vekta/object/module/TorpedoModule.java
@@ -1,6 +1,8 @@
 package vekta.object.module;
 
+import processing.core.PApplet;
 import processing.core.PVector;
+import vekta.Vekta;
 import vekta.object.HomingProjectile;
 import vekta.object.ModularShip;
 import vekta.object.SpaceObject;
@@ -11,8 +13,11 @@ import static vekta.Vekta.getWorld;
 
 public class TorpedoModule extends WeaponModule {
 	private final float speed;
+	private final float RADIUS = 1;
+	private Vekta v;
 	
 	public TorpedoModule(float speed) {
+		v = Vekta.getInstance();
 		this.speed = speed;
 	}
 
@@ -38,7 +43,7 @@ public class TorpedoModule extends WeaponModule {
 			SpaceObject target = ((Targeter)m).getTarget();
 			if(target != null && ship.consumeEnergy(1)) {
 				getWorld().playSound("laser", ship.getPosition());
-				PVector velocity = ship.getVelocity().add(PVector.random2D());
+				PVector velocity = ship.getHeading().add(PVector.random2D()).add(ship.getVelocity());
 				addObject(new HomingProjectile(ship, target, getSpeed(), ship.getPosition(), velocity, ship.getColor()));
 			}
 		}
