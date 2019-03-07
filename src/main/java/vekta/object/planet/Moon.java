34aa443a84c2200dd7bed28c446b0832e24b738a
Ryan Vandersmith <ryanvandersmith@gmail.com>
2019-03-06 21:22:01 -0700

    Add moon trails around parent planet

--- src/main/java/vekta/object/planet/Moon.java
+++ src/main/java/vekta/object/planet/Moon.java
@@ -2,8 +2,11 @@ package vekta.object.planet;
 
 import processing.core.PVector;
 import vekta.Counter;
+import vekta.RenderLevel;
 import vekta.WorldGenerator;
 import vekta.terrain.Terrain;
+
+import static vekta.Vekta.getWorld;
 
 public class Moon extends TerrestrialPlanet {
 
@@ -12,7 +15,8 @@ public class Moon extends TerrestrialPlanet {
 
 	private final Counter orbitCt = new Counter(10).randomize();
 
-//	private final PVector lastParentPosition = new PVector();
+	private final PVector lastParentPosition = new PVector();
+	private float lastTimeScale = 1;
 
 	public Moon(Planet parent, String name, float mass, float density, Terrain terrain, PVector position, PVector velocity, int color) {
 		super(name, mass, density, terrain, position, velocity, color);
@@ -46,36 +50,33 @@ public class Moon extends TerrestrialPlanet {
 	}
 
 	@Override
-	public int getTrailLength() {
-		return 1;
+	public void draw(RenderLevel level, float r) {
+		if(RenderLevel.AROUND_PLANET.isVisibleTo(level)) {
+			super.draw(level, r);
+		}
 	}
 
-//	@Override
-//	public void draw(RenderLevel level, float r) {
-//		if(RenderLevel.AROUND_PLANET.isVisibleTo(level)) {
-//			super.draw(level, r);
-//		}
-//	}
-//
-//	@Override
-//	public void updateTrail() {
-//		// Sets trail relative to parent. TODO: generalize
-//		super.updateTrail();
-//
-//		PVector parentPos = getParent().getPosition();
-////		PVector offset = parentPos.copy().sub(lastParentPosition);
-//		for(int i = 0; i < trail.length; i++) {
-//			PVector pos = trail[i];
-//			if(pos != null) {
-//				pos.sub(getParent().getVelocity().div(getWorld().getTimeScale()));
-//			}
-//		}
-////		lastParentPosition.set(parentPos);
-//	}
+	@Override
+	public void updateTrail() {
+		// Sets trail relative to parent. TODO: generalize
+		super.updateTrail();
 
-//	@Override
-//	public void updateOrigin(PVector offset) {
-//		super.updateOrigin(offset);
-//		lastParentPosition.add(offset);
-//	}
+		float timeScale = getWorld().getTimeScale();
+		PVector parentPos = getParent().getPosition();
+		PVector offset = parentPos.copy().sub(lastParentPosition).mult(timeScale / lastTimeScale);
+		for(int i = 0; i < trail.length; i++) {
+			PVector pos = trail[i];
+			if(pos != null) {
+				pos.add(offset);
+			}
+		}
+		lastParentPosition.set(parentPos);
+		lastTimeScale = timeScale;
+	}
+
+	@Override
+	public void updateOrigin(PVector offset) {
+		super.updateOrigin(offset);
+		lastParentPosition.add(offset);
+	}
 }
