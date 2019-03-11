3dd8e1443f0fa9e1edce2d865fa6e60659047bad
StGerGer <nateestes223@gmail.com>
2019-03-11 17:29:23 -0600

    Added structuralModule design

--- src/main/java/vekta/module/station/StructuralModule.java
+++ src/main/java/vekta/module/station/StructuralModule.java
@@ -1,10 +1,14 @@
 package vekta.module.station;
 
+import vekta.RenderLevel;
 import vekta.module.Module;
 import vekta.module.ModuleType;
 import vekta.module.ShipModule;
 import vekta.object.ship.ModularShip;
 import vekta.object.ship.SpaceStation;
+
+import static processing.core.PConstants.CORNERS;
+import static vekta.Vekta.v;
 
 public class StructuralModule extends ShipModule {
 	private final int width, height;
@@ -35,7 +39,7 @@ public class StructuralModule extends ShipModule {
 
 	@Override
 	public Module getVariant() {
-		return new StructuralModule(chooseInclusive(1, 3), chooseInclusive(1, 3));
+		return new StructuralModule(chooseInclusive(5, 10), chooseInclusive(10, 20));
 	}
 
 	@Override
@@ -51,5 +55,29 @@ public class StructuralModule extends ShipModule {
 	@Override
 	public int getHeight() {
 		return height;
+	}
+
+	@Override
+	public void draw(RenderLevel dist, float tileSize) {
+
+		v.rectMode(CORNERS);
+		v.fill(0);
+		v.translate(-(getWidth() / 2F) * tileSize, -(getHeight() / 2F) * tileSize);
+
+		// Cross supports
+		for(int i = 0; i < getWidth(); i++) {
+			v.line(i * tileSize, 2, (i + 1) * tileSize, getHeight() * tileSize - 2);
+			v.line(i * tileSize, getHeight() * tileSize - 2, (i + 1) * tileSize, 2);
+		}
+
+		// Endcaps
+		v.rect(0, 0, .1F * tileSize, getHeight() * tileSize);
+		v.rect(getWidth() * tileSize, 0, (getWidth() - .1F) * tileSize, getHeight() * tileSize);
+
+		// Long supports
+		v.rect(0, 0, getWidth() * tileSize, (getHeight() * .1F) * tileSize);
+		v.rect(0, getHeight() * tileSize, getWidth() * tileSize, (getHeight() - (getHeight() * .1F)) * tileSize);
+
+
 	}
 }
