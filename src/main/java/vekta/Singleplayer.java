1634646d8210783758d102c10fce446f60440bb1
Ryan Vandersmith <ryanvandersmith@gmail.com>
2019-03-13 16:15:26 -0600

    Revert change

--- src/main/java/vekta/Singleplayer.java
+++ src/main/java/vekta/Singleplayer.java
@@ -230,12 +230,6 @@ public class Singleplayer implements World, PlayerListener {
 			timeScale = MIN_PLANET_TIME_SCALE;
 		}
 
-		// Counteract velocity mismatch on player zoom
-		if(prevTimeScale != timeScale) {
-			playerShip.getPositionReference()
-					.add(playerShip.getVelocity().mult(timeScale - prevTimeScale));
-		}
-		
 		updateGlobal(level);
 
 		v.clear();
