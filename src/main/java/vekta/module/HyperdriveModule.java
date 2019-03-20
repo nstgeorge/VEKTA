8405dfd2609e51115adec71eb27470db876db0a2
Ryan Vandersmith <ryanvandersmith@gmail.com>
2019-03-19 18:21:13 -0600

    More hyperdrive tweaks

--- src/main/java/vekta/module/HyperdriveModule.java
+++ src/main/java/vekta/module/HyperdriveModule.java
@@ -10,8 +10,8 @@ import static vekta.Vekta.*;
 
 public class HyperdriveModule extends ShipModule {
 	private static final float LOW_TIME_SCALE_SPEEDUP = 50;
-	private static final float MIN_SPEED = 10;
-	private static final float MAX_SPEED = 50;
+	private static final float MIN_BOOST = 10;
+	private static final float MAX_BOOST = 100;
 
 	private final float boost;
 
@@ -65,26 +65,26 @@ public class HyperdriveModule extends ShipModule {
 
 		float timeScale = getWorld().getTimeScale();
 		float thrust = ship.isLanding() ? -1 : ship.getThrustControl();
-		currentBoost = max(0, min(MAX_SPEED, ship.getVelocity().dot(ship.getHeading()))) * getBoost();
+		currentBoost = max(0, min(MAX_BOOST, ship.getVelocity().dot(ship.getHeading()))) * getBoost();
 
-		boolean movingFast = ship.getVelocity().magSq() >= sq(MIN_SPEED * timeScale);
+		boolean movingFast = ship.getVelocity().magSq() >= sq(MIN_BOOST * timeScale);
 		if((!movingFast && thrust < 0) || !ship.hasEnergy()) {
 			endHyperdrive();
 		}
 
 		if(isActive() && ship.consumeEnergyOverTime(.05F * currentBoost * PER_SECOND)) {
 			float effectiveThrust = thrust * max(timeScale, LOW_TIME_SCALE_SPEEDUP) / timeScale;
-
-			ship.setVelocity(ship.getHeading().setMag(ship.getVelocity().mag()));
+			ship.setVelocity(ship.getHeading().setMag(min(currentBoost * timeScale, ship.getVelocity().mag())));
 			ship.accelerate(effectiveThrust * currentBoost, ship.getVelocity());
 
+			// Create shockwave effect
 			Shockwave wave = register(new Shockwave(
 					getShip(),
-					timeScale * 1e-4F * currentBoost,
+					timeScale * 5e-5F * currentBoost * thrust,
 					(int)v.random(20, 40),
 					getShip().getColor()));
 			wave.setRadius(1);
-			wave.addVelocity(ship.getVelocity().mult(-.1F));
+			wave.addVelocity(ship.getHeading().mult(-1000 * timeScale / currentBoost * thrust));
 		}
 	}
 
