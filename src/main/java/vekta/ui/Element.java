e464df307939a0d049a36f03d93e3e4a19633100
Juan Becerra <juanbecerra@u.boisestate.edu>
2020-12-09 08:33:25 -0700

    Fixed serialization error on Element.java (#52)

--- src/main/java/vekta/ui/Element.java
+++ src/main/java/vekta/ui/Element.java
@@ -1,9 +1,11 @@
 package vekta.ui;
 
+import java.io.Serializable;	// Allows classes that implement Element interface to be serializable
+
 /**
  * A minimal interface that every UI element inherits from.
  */
-public interface Element {
+public interface Element extends Serializable {
 	/**
 	 * Draw and update element
 	 */
