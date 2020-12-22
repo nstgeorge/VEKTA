3ff82be9fb96ed9ad5627f37db3b8577e8d4a02a
Ryan Vandersmith <ryanvandersmith@gmail.com>
2020-12-21 17:00:24 -0700

    Refactor KeyBinding/KeyCategory

---
+++ src/main/java/vekta/KeyCategory.java
@@ -0,0 +1,8 @@
+package vekta;
+
+public enum KeyCategory {
+	SHIP_CONTROLS,
+	SHIP_COMPUTER,
+	QUICK_ACTIONS,
+	MENU_CONTROLS
+}
--- src/main/java/vekta/menu/option/KeyCategoryButton.java
+++ src/main/java/vekta/menu/option/KeyCategoryButton.java
@@ -1,32 +1,45 @@
 package vekta.menu.option;
 
 import vekta.KeyBinding;
+import vekta.KeyCategory;
 import vekta.menu.Menu;
 import vekta.menu.handle.KeybindMenuHandle;
 import vekta.menu.option.input.InputOption;
 import vekta.menu.option.input.KeyBindingInputController;
 import vekta.menu.option.input.KeySettingWatcher;
 
+import java.util.Locale;
+
 import static vekta.Vekta.setContext;
 import static vekta.Vekta.v;
 
-public class KeybindMenuSubsectionButton extends ButtonOption {
+public class KeyCategoryButton extends ButtonOption {
 
-	private String section;
+	private final KeyCategory category;
+	private final String name;
 
-	public KeybindMenuSubsectionButton(String section) {
-		this.section = section;
+	public KeyCategoryButton(KeyCategory category) {
+		this.category = category;
+
+		// Cache name
+		String name = category.name().replace("_", " ");
+		this.name = name.charAt(0) + name.substring(1).toLowerCase();
 	}
+
 	@Override
 	public String getName() {
-		return section;
+		return name;
+	}
+
+	public KeyCategory getCategory() {
+		return category;
 	}
 
 	@Override
 	public void onSelect(Menu menu) {
-		Menu sub = new Menu(menu, new KeybindMenuHandle(section));
+		Menu sub = new Menu(menu, new KeybindMenuHandle(getName()));
 		for(KeyBinding key : KeyBinding.values()) {
-			if(key.getSection().equals(section.replace(" ", "_").toUpperCase())) {
+			if(key.getCategory() == getCategory()) {
 				sub.add(new InputOption<>(
 						key.name().replace("_", " ").toLowerCase(),
 						new KeySettingWatcher(key),
